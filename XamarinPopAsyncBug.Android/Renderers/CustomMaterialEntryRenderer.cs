5b15998cc257e82d8bdf8e4450267e571ff92b84
elicul <elicul@symmetria-consulting.hr>
2021-01-08 17:09:06 +0100

    Custom renderer added

---
+++ XamarinPopAsyncBug.Android/Renderers/CustomMaterialEntryRenderer.cs
@@ -0,0 +1,41 @@
+﻿using Android.Content;
+using Google.Android.Material.TextField;
+using Xamarin.Forms;
+using Xamarin.Forms.Material.Android;
+using Xamarin.Forms.Platform.Android;
+using XamarinPopAsyncBug;
+using XamarinPopAsyncBug.Droid.Renderers;
+
+[assembly: ExportRenderer(typeof(CustomMaterialEntry), typeof(CustomMaterialEntryRenderer), new[] { typeof(VisualMarker.MaterialVisual) })]
+
+namespace XamarinPopAsyncBug.Droid.Renderers
+{
+    public class CustomMaterialEntryRenderer : MaterialEntryRenderer
+    {
+        private CustomMaterialEntry _customMaterialEntry;
+        private Context _context;
+
+        public CustomMaterialEntryRenderer(Context context) : base(context)
+        {
+            _context = context;
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null || e.NewElement == null) return;
+
+            _customMaterialEntry = (CustomMaterialEntry)Element;
+            Control.HintEnabled = true;
+            Control.HelperTextEnabled = true;
+            Control.SetBoxCornerRadii(10f, 10f, 10f, 10f);
+            Control.BoxBackgroundMode = TextInputLayout.BoxBackgroundOutline;
+        }
+
+        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+        }
+    }
+}
---
+++ XamarinPopAsyncBug/CustomMaterialEntry.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinPopAsyncBug
+{
+    public class CustomMaterialEntry : Entry
+    {
+        public static readonly BindableProperty HasErrorProperty =
+  BindableProperty.Create(nameof(HasError), typeof(bool), typeof(CustomMaterialEntry), false);
+
+        public bool HasError
+        {
+            get { return (bool)GetValue(HasErrorProperty); }
+            set { SetValue(HasErrorProperty, value); }
+        }
+
+        public static readonly BindableProperty ErrorTextProperty =
+  BindableProperty.Create(nameof(ErrorText), typeof(string), typeof(CustomMaterialEntry), string.Empty);
+
+        public string ErrorText
+        {
+            get { return (string)GetValue(ErrorTextProperty); }
+            set { SetValue(ErrorTextProperty, value); }
+        }
+    }
+}
